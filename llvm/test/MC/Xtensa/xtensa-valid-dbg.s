7e19a5812489e6d21f4101703a4badad9f4e1ab8
Andrei Safronov <safronov@espressif.com>
2020-06-03 16:41:53 +0300

    [Xtensa] Fixes break instruction reversed operands.
    Closes #25

---
+++ llvm/test/MC/Xtensa/xtensa-valid-dbg.s
@@ -0,0 +1,10 @@
+# RUN: llvm-mc %s -triple=xtensa -mcpu=esp32 -show-encoding \
+# RUN:     | FileCheck -check-prefixes=CHECK,CHECK-INST %s
+
+
+.align	4
+LBL0:
+
+# CHECK-INST:  break     1, 2
+# CHECK: encoding: [0x20,0x41,0x00]
+ break 1, 2
