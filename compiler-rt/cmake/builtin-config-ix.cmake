b24636f7cfccef9b6824ceccce1ef24406e9f1ad
Andrei Safronov <safronov@espressif.com>
2021-12-30 11:09:36 +0300

    [Xtensa] Initial porting compiler-rt library for Xtensa.

--- compiler-rt/cmake/builtin-config-ix.cmake
+++ compiler-rt/cmake/builtin-config-ix.cmake
@@ -50,6 +50,7 @@ set(SPARC sparc)
 set(SPARCV9 sparcv9)
 set(WASM32 wasm32)
 set(WASM64 wasm64)
+set(XTENSA xtensa)
 set(VE ve)
 
 if(APPLE)
@@ -62,7 +63,7 @@ set(ALL_BUILTIN_SUPPORTED_ARCH
   ${X86} ${X86_64} ${ARM32} ${ARM64}
   ${HEXAGON} ${MIPS32} ${MIPS64} ${PPC32} ${PPC64}
   ${RISCV32} ${RISCV64} ${SPARC} ${SPARCV9}
-  ${WASM32} ${WASM64} ${VE})
+  ${WASM32} ${WASM64} ${VE} ${XTENSA})
 
 include(CompilerRTUtils)
 include(CompilerRTDarwinUtils)
