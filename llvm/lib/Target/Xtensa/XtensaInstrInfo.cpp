9dee81fbeafe78cc0a455f3dcb4208722875314e
Andrei Safronov <safronov@espressif.com>
2022-04-21 10:08:03 +0300

    [Xtensa] Correction of the Hardware Loop pass.
    Update loop counter via a phi instruction. This
    improvement fix case when loop have multiple enters.

--- llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
+++ llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
@@ -260,6 +260,7 @@ bool XtensaInstrInfo::reverseBranchCondition(
     return false;
 
   case Xtensa::LOOPEND:
+  case Xtensa::LOOPBR:
     return true;
 
   default:
@@ -297,6 +298,7 @@ XtensaInstrInfo::getBranchDestBlock(const MachineInstr &MI) const {
   case Xtensa::BNEZ:
   case Xtensa::BLTZ:
   case Xtensa::BGEZ:
+  case Xtensa::LOOPBR:
     return MI.getOperand(1).getMBB();
 
   case Xtensa::BT:
@@ -317,6 +319,7 @@ bool XtensaInstrInfo::isBranchOffsetInRange(unsigned BranchOp,
   case Xtensa::JX:
     return true;
   case Xtensa::LOOPEND:
+  case Xtensa::LOOPBR:
     BrOffset += 4;
     assert((BrOffset <= 0) && "Wrong hardware loop");
     return true;
@@ -632,6 +635,7 @@ unsigned XtensaInstrInfo::InsertBranchAtInst(MachineBasicBlock &MBB,
   case Xtensa::BNEZ:
   case Xtensa::BLTZ:
   case Xtensa::BGEZ:
+  case Xtensa::LOOPBR:
     MI = BuildMI(MBB, I, DL, get(BR_C)).addReg(Cond[1].getReg()).addMBB(TBB);
     break;
   case Xtensa::BT:
@@ -639,7 +643,7 @@ unsigned XtensaInstrInfo::InsertBranchAtInst(MachineBasicBlock &MBB,
     MI = BuildMI(MBB, I, DL, get(BR_C)).addReg(Cond[1].getReg()).addMBB(TBB);
     break;
   case Xtensa::LOOPEND:
-    MI = BuildMI(MBB, I, DL, get(BR_C)).addMBB(TBB);
+    MI = BuildMI(MBB, I, DL, get(Xtensa::LOOPEND)).addMBB(TBB);
     break;
   default:
     llvm_unreachable("Invalid branch type!");
@@ -728,6 +732,7 @@ bool XtensaInstrInfo::isBranch(const MachineBasicBlock::iterator &MI,
   case Xtensa::BNEZ:
   case Xtensa::BLTZ:
   case Xtensa::BGEZ:
+  case Xtensa::LOOPBR:
     Cond[0].setImm(OpCode);
     Target = &MI->getOperand(1);
     return true;
